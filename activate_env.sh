ebed3c42deec4f77038d831e875ed7b7b7c73135
Sithael <chojnacki.jamie@gmail.com>
2023-11-11 19:11:01 +0100

    Disable tests due to changes in directories

--- activate_env.sh
+++ activate_env.sh
@@ -8,5 +8,5 @@ source $(poetry env info --path)/bin/activate
 pre-commit install
 echo "MARL Engineering Activated"
 
-echo "Running Tests..."
-pytest ./tests -W ignore::DeprecationWarning
+# echo "Running Tests..."
+# pytest ./tests -W ignore::DeprecationWarning
