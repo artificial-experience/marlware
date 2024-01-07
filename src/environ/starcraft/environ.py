232f6185f8c71ee95043c4f78fb29bf15e8030aa
Sithael <chojnacki.jamie@gmail.com>
2024-01-07 12:24:46 +0100

    fix seed for env

--- src/environ/starcraft/environ.py
+++ src/environ/starcraft/environ.py
@@ -21,20 +21,10 @@ class SC2Environ:
     def __init__(self, map_name: str) -> None:
         self._map_name = map_name
 
-    def _rnd_seed(self, *, seed: Optional[int] = None):
-        """set random seed"""
-        if seed:
-            torch.manual_seed(seed)
-            if torch.cuda.is_available():
-                torch.cuda.manual_seed(seed)
-            np.random.seed(seed)
-            random.seed(seed)
-
     def create_env_instance(
         self, *, seed: Optional[int] = None
     ) -> Tuple[StarCraft2Env, Dict]:
         """create sc2 environ based on passed map name and return along with info"""
-        self._rnd_seed(seed=seed)
-        env = StarCraft2Env(map_name=self._map_name)
+        env = StarCraft2Env(map_name=self._map_name, seed=seed)
         env_info = env.get_env_info()
         return env, env_info
